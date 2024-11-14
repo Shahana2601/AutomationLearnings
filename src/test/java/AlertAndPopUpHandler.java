be8cac755a32df57f72b6c70dced4a69112445c5
Shahana2601 <shahanalfajar@gmail.com>
2024-11-14 15:02:32 +0530

    created fileUploadHandler to handle uploads

--- src/test/java/AlertAndPopUpHandler.java
+++ src/test/java/AlertAndPopUpHandler.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class AlertAndPopUpHandle {
+public class AlertAndPopUpHandler {
 
     @Test
     void handleAlertBoxPopUp() throws InterruptedException {
@@ -19,13 +19,12 @@ public class AlertAndPopUpHandle {
         Alert alert = chromeDriver.switchTo().alert();
         System.out.println(alert.getText());
         alert.accept();
-
         //chromeDriver.close();
     }
 
     @Test
     void handleConfirmBoxPopUp() throws InterruptedException {
-        WebDriver chromeDriver =  new ChromeDriver();
+        WebDriver chromeDriver = new ChromeDriver();
         chromeDriver.get("https://manojkumar4636.github.io/Selenium_Practice_Hub/home.html");
 
         chromeDriver.findElement(By.xpath("//h5[text()='Alert']//parent::a")).click();
@@ -35,6 +34,22 @@ public class AlertAndPopUpHandle {
 
         Alert alert = chromeDriver.switchTo().alert();
         System.out.println(alert.getText());
+        alert.accept();
+    }
+
+    @Test
+    void handlePromptBoxPopUp() throws InterruptedException {
+        WebDriver chromeDriver = new ChromeDriver();
+        chromeDriver.get("https://manojkumar4636.github.io/Selenium_Practice_Hub/home.html");
+
+        chromeDriver.findElement(By.xpath("//h5[text()='Alert']//parent::a")).click();
+        chromeDriver.findElement(By.xpath("//button[@onClick='confirmPrompt()']")).click();
+
+        Thread.sleep(5000);
+
+        Alert alert = chromeDriver.switchTo().alert();
+        System.out.println(alert.getText());
+        alert.sendKeys("test");
         alert.accept();
     }
 }
