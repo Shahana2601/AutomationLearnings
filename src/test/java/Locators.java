a7a2dabf0251bd976c6bf13f6fd08a3e7d70fefd
Shahana2601 <shahanalfajar@gmail.com>
2024-11-14 11:09:07 +0530

    More locators were found by creating xpaths

--- src/test/java/Locators.java
+++ src/test/java/Locators.java
@@ -9,11 +9,15 @@ public class Locators {
     @Test
     void findLocators(){
         WebDriver chromeDriver = new ChromeDriver();
-        chromeDriver.get("https://demo.nopcommerce.com/login?returnUrl=%2F");
+        chromeDriver.get("https://manojkumar4636.github.io/Selenium_Practice_Hub/home.html");
 
-        chromeDriver.findElement(By.xpath("//*[@id=\"Email\"]")).sendKeys("sha123@gmail.com");
-        chromeDriver.findElement(By.xpath("//*[@id=\"Password\"]")).sendKeys("sha123");
+        chromeDriver.findElement(By.xpath("//h5[text()='Edit']//parent::a")).click();
+        chromeDriver.findElement(By.xpath("//input[@id='email']")).sendKeys("sha123@gmail.com");
+        chromeDriver.findElement(By.xpath("//input[@type='text' and @value='Append ']")).sendKeys("hey this is me");
 
-        chromeDriver.findElement(By.xpath("//*[@id=\"main\"]/div/div/div/div[2]/div[1]/div[2]/form/div[3]/button")).click();
+        chromeDriver.findElement(By.xpath("//input[@type='text' and @value='TestLeaf']")).sendKeys("testLeaf");
+        chromeDriver.findElement(By.xpath("//input[@type='text' and @value='Clear me!!']")).sendKeys("clear it");
+
+        chromeDriver.close();
     }
 }
