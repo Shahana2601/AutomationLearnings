f79728d8464f4b031640af5d427877af7eeee28d
Shahana2601 <shahanalfajar@gmail.com>
2024-11-15 11:06:56 +0530

    added code for nested frames.

--- src/test/java/FrameHandler.java
+++ src/test/java/FrameHandler.java
@@ -22,8 +22,8 @@ public class FrameHandler {
         WebDriver chromeDriver = new ChromeDriver();
         chromeDriver.get("https://manojkumar4636.github.io/Selenium_Practice_Hub/home.html");
         chromeDriver.findElement(By.xpath("//h5[text()='Frame']//parent::a")).click();
-        //chromeDriver.switchTo().frame("wrapframe");
         chromeDriver.switchTo().frame(chromeDriver.findElement(By.xpath("//div[@id='wrapframe']//child::iframe[@src='page.html']")));
+        chromeDriver.switchTo().frame(chromeDriver.findElement(By.xpath("//iframe[@id='frame2' and @src='nested.html']")));
 
         Thread.sleep(3000);
         chromeDriver.findElement(By.xpath("//button[@id='Click1']")).click();
