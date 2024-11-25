433e0758fb37fc899ae1f9ebd037a0ef57096de0
Shahana2601 <shahanalfajar@gmail.com>
2024-11-25 18:07:11 +0530

    Created class for launching chrome using testNg

---
+++ src/test/java/com/testng/ChromeLauncherTest.java
@@ -0,0 +1,48 @@
+package com.testng;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class ChromeLauncherTest {
+
+    WebDriver chromeDriver;
+
+    @BeforeMethod
+    public void setUP(){
+        System.setProperty("webdriver.chrome.driver" , "chromedriver.exe");
+        chromeDriver = new ChromeDriver();
+        chromeDriver.manage().window().maximize();
+        chromeDriver.manage().deleteAllCookies();
+        chromeDriver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        chromeDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        chromeDriver.get("https://google.com");
+    }
+
+    @Test(priority = 1)
+    public void testChromeTitle(){
+        String browserTitle = chromeDriver.getTitle();
+        System.out.println(browserTitle);
+    }
+
+    @Test(priority = 2)
+    public void mailLinkTest(){
+        boolean b = chromeDriver.findElement(By.linkText("Gmail")).isDisplayed();
+    }
+
+    @Test(priority = 3)
+    public void logoDisplayTest(){
+        boolean logo = chromeDriver.findElement(By.className("lnXdpd")).isDisplayed();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        chromeDriver.quit();
+    }
+}
---
+++ src/test/java/com/testng/LaunchTest.java
@@ -0,0 +1,17 @@
+package com.testng;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LaunchTest {
+
+    @Test
+    void launchChrome(){
+        WebDriver chromeDriver = new ChromeDriver();
+        chromeDriver.get("https://google.com");
+        System.out.println(chromeDriver.getTitle());
+
+        chromeDriver.quit();
+    }
+}
