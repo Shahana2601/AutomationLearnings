e951a4a38fe67dd8757a42b2b20d1e4f8b38205b
Shahana2601 <shahanalfajar@gmail.com>
2024-12-03 11:11:57 +0530

    changes in xml file added

--- src/test/java/com/test/retrytest/TestTransformer.java
+++ src/test/java/com/test/retrytest/TestTransformer.java
@@ -8,6 +8,7 @@ import java.lang.reflect.Method;
 
 public class TestTransformer implements IAnnotationTransformer {
 
+    @Override
     public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod){
         annotation.setRetryAnalyzer(RetryAnalyzer.class);
     }
